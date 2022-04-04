75dd16ad09c75e3f26fa9e4efbc76ebf811bdb28
Adam Harper <aharper8484@gmail.com>
2022-04-04 18:18:26 +0100

    added decryption function but needs amending/reversing

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ const slowRotor = [
 ];
 
 //reflector array
-const reflector = [
+const reflectorA = [
   "E",
   "J",
   "M",
@@ -176,15 +176,152 @@ const reflector = [
   "D",
 ];
 
+const reflectorB = [
+  "Y",
+  "R",
+  "U",
+  "H",
+  "Q",
+  "S",
+  "L",
+  "D",
+  "P",
+  "X",
+  "N",
+  "G",
+  "O",
+  "K",
+  "M",
+  "I",
+  "E",
+  "B",
+  "F",
+  "Z",
+  "C",
+  "W",
+  "V",
+  "J",
+  "A",
+  "T",
+];
+
 // retrieve users message and encrypt it
-function getMessage() {
+function encrypt() {
   //variables containing rotor positions
-  let fastRotorOffset = 0;
-  let mediumRotorOffset = 0;
-  let slowRotorOffset = 0;
+  let fastRotorOffset = document.getElementById("r1").value;
+  let fastCountStart = fastRotorOffset;
+  let mediumRotorOffset = document.getElementById("r2").value;
+  let mediumCountStart = mediumRotorOffset;
+  let slowRotorOffset = document.getElementById("r3").value;
+  let slowcountStart = slowRotorOffset;
+
+  let reflectorChoice = document.getElementById("reflector").value;
+  if (reflectorChoice === "A") {
+    reflectorArray = reflectorA;
+  } else {
+    reflectorArray = reflectorB;
+  }
 
   //empty variable to add encrypted letters to
   let encryptedMsg = "";
+
+  //function to retrieve text, remove space + non-letter characters and convert to uppercase
+  var userInput = document
+    .getElementById("encryption")
+    .value.replace(/\s+/g, "")
+    .toUpperCase();
+
+  //loop through each letter of the message/user input
+  for (let i = 0; i < userInput.length; i++) {
+    //plugboard Outbound
+    //get index of letter form alphabet and store in variable
+    let plugboardInputIndex = alphabet.indexOf(userInput[i]);
+    //substitute letter with plugboard array
+    let plugboardOutput = plugboard[plugboardInputIndex];
+
+    // sample messages
+    // hwjefmcwltfnsnselrfdnwoehwjefmcwltfnsnselrfdnwoehwjefmcwltfnsnselrfdnwoe
+    // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    //fast rotor substitute
+    //get index of the plugboard array
+    let fastRotorOut = plugboard.indexOf(plugboardOutput);
+    //substitute letter with fast rotor array
+    //include offset
+    fastRotorOut = (fastRotorOut + fastRotorOffset) % 26;
+    let fastRotorNewLetter = fastRotor[fastRotorOut];
+    let fastRotorNewIndex = fastRotor.indexOf(fastRotorNewLetter);
+    fastRotorNewIndex = (fastRotorNewIndex - fastRotorOffset + 26) % 26;
+    fastRotorOffset++;
+
+    //rotor2 sub and rotate
+    let mediumRotorOut = fastRotor.indexOf(fastRotorNewLetter);
+    mediumRotorOut = (mediumRotorOut + mediumRotorOffset) % 26;
+    let mediumRotorNewLetter = mediumRotor[mediumRotorOut];
+    let mediumRotorNewIndex = mediumRotor.indexOf(mediumRotorNewLetter);
+    mediumRotorNewIndex = (mediumRotorNewIndex - mediumRotorOffset + 26) % 26;
+    if (fastRotorOffset > 0 && (fastRotorOffset - fastCountStart) % 26 == 0) {
+      mediumRotorOffset++;
+    }
+
+    //rotor3 sub and rotate
+    let slowRotorOut = mediumRotor.indexOf(mediumRotorNewLetter);
+    slowRotorOut = (slowRotorOut + slowRotorOffset) % 26;
+    let slowRotorNewLetter = slowRotor[slowRotorOut];
+    let slowRotorNewIndex = slowRotor.indexOf(slowRotorNewLetter);
+    slowRotorNewIndex = (slowRotorNewIndex - slowRotorOffset + 26) % 26;
+    if (
+      mediumRotorOffset > 0 &&
+      (mediumRotorOffset - mediumCountStart) % 26 == 0
+    ) {
+      slowRotorOffset++;
+    }
+
+    //reflector substitution
+    let reflectorInputIndex = slowRotor.indexOf(slowRotorNewLetter);
+    //substitute letter with reflector array *REFLECTOR*
+    let reflectorNewLetter = reflectorArray[reflectorInputIndex];
+
+    //rotor3-substitute only *REFLECTOR*
+    let slowRotorReturnIndex = reflectorArray.indexOf(reflectorNewLetter);
+    let slowRotorReturnLetter = slowRotor[slowRotorReturnIndex];
+
+    //rotor2-substitute only
+    let mediumRotorReturnIndex = slowRotor.indexOf(slowRotorReturnLetter);
+    let mediumRotorReturnLetter = mediumRotor[mediumRotorReturnIndex];
+
+    //rotor1-substitute only
+    let fastRotorReturnIndex = mediumRotor.indexOf(mediumRotorReturnLetter);
+    let fastRotorReturnLetter = fastRotor[fastRotorReturnIndex];
+
+    //plugboard
+    let plugboardReturnIndex = fastRotor.indexOf(fastRotorReturnLetter);
+    let cypherLetter = plugboard[plugboardReturnIndex];
+
+    //push resulting letter to cypherOutput
+    encryptedMsg = encryptedMsg + cypherLetter;
+  }
+  return (document.getElementById("message").innerHTML = encryptedMsg);
+}
+
+function decrypt() {
+  //variables containing rotor positions
+  let fastRotorOffset = document.getElementById("r1").value;
+  let fastCountStart = fastRotorOffset;
+  let mediumRotorOffset = document.getElementById("r2").value;
+  let mediumCountStart = mediumRotorOffset;
+  let slowRotorOffset = document.getElementById("r3").value;
+  let slowcountStart = slowRotorOffset;
+
+  let reflectorChoice = document.getElementById("reflector").value;
+  if (reflectorChoice === "A") {
+    reflectorArray = reflectorA;
+  } else {
+    reflectorArray = reflectorB;
+  }
+
+  //empty variable to add encrypted letters to
+  let decryptedMsg = "";
 
   //function to retrieve text, remove space + non-letter characters and convert to uppercase
   var userInput = document
@@ -221,7 +358,7 @@ function getMessage() {
     let mediumRotorNewLetter = mediumRotor[mediumRotorOut];
     let mediumRotorNewIndex = mediumRotor.indexOf(mediumRotorNewLetter);
     mediumRotorNewIndex = (mediumRotorNewIndex - mediumRotorOffset + 26) % 26;
-    if (fastRotorOffset > 0 && fastRotorOffset % 26 == 0) {
+    if (fastRotorOffset > 0 && (fastRotorOffset - fastCountStart) % 26 == 0) {
       mediumRotorOffset++;
     }
 
@@ -231,40 +368,38 @@ function getMessage() {
     let slowRotorNewLetter = slowRotor[slowRotorOut];
     let slowRotorNewIndex = slowRotor.indexOf(slowRotorNewLetter);
     slowRotorNewIndex = (slowRotorNewIndex - slowRotorOffset + 26) % 26;
-    if (mediumRotorOffset > 0 && mediumRotorOffset % 26 == 0) {
+    if (
+      mediumRotorOffset > 0 &&
+      (mediumRotorOffset - mediumCountStart) % 26 == 0
+    ) {
       slowRotorOffset++;
     }
 
     //reflector substitution
     let reflectorInputIndex = slowRotor.indexOf(slowRotorNewLetter);
-    //substitute letter with reflector array
-    let reflectorNewLetter = reflector[reflectorInputIndex];
-    console.log("reflector: " + reflectorNewLetter);
+    //substitute letter with reflector array *REFLECTOR*
+    let reflectorNewLetter = reflectorArray[reflectorInputIndex];
 
-    //rotor3-substitute only
-    let slowRotorReturnIndex = reflector.indexOf(reflectorNewLetter);
+    //rotor3-substitute only *REFLECTOR*
+    let slowRotorReturnIndex = reflectorArray.indexOf(reflectorNewLetter);
     let slowRotorReturnLetter = slowRotor[slowRotorReturnIndex];
-    console.log("R3: " + slowRotorReturnLetter);
 
     //rotor2-substitute only
     let mediumRotorReturnIndex = slowRotor.indexOf(slowRotorReturnLetter);
     let mediumRotorReturnLetter = mediumRotor[mediumRotorReturnIndex];
-    console.log("R2: " + mediumRotorReturnLetter);
 
     //rotor1-substitute only
     let fastRotorReturnIndex = mediumRotor.indexOf(mediumRotorReturnLetter);
     let fastRotorReturnLetter = fastRotor[fastRotorReturnIndex];
-    console.log("R1: " + fastRotorReturnLetter);
 
     //plugboard
     let plugboardReturnIndex = fastRotor.indexOf(fastRotorReturnLetter);
     let cypherLetter = plugboard[plugboardReturnIndex];
-    console.log("plug: " + cypherLetter);
 
     //push resulting letter to cypherOutput
-    encryptedMsg = encryptedMsg + cypherLetter;
+    decryptedMsg = decryptedMsg + cypherLetter;
   }
-  return (document.getElementById("cypher").innerHTML = encryptedMsg);
+  return (document.getElementById("message").innerHTML = decryptedMsg);
 }
 
 //push encryptedMsg onto index.html
